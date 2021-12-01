8fde3d7353cd91c211f084f5652057107925ceba
ragy2801 <92475336+ragy2801@users.noreply.github.com>
2021-12-01 15:24:26 -0500

    Add files via upload

---
+++ bitmap/cmake-build-debug/CMakeFiles/bitmap.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/ragycostadejesus/Downloads/bitmap/BMCoreHeader.cpp" "/Users/ragycostadejesus/Downloads/bitmap/cmake-build-debug/CMakeFiles/bitmap.dir/BMCoreHeader.cpp.o"
+  "/Users/ragycostadejesus/Downloads/bitmap/BMFile.cpp" "/Users/ragycostadejesus/Downloads/bitmap/cmake-build-debug/CMakeFiles/bitmap.dir/BMFile.cpp.o"
+  "/Users/ragycostadejesus/Downloads/bitmap/BMFileHeader.cpp" "/Users/ragycostadejesus/Downloads/bitmap/cmake-build-debug/CMakeFiles/bitmap.dir/BMFileHeader.cpp.o"
+  "/Users/ragycostadejesus/Downloads/bitmap/BMInfoHeader.cpp" "/Users/ragycostadejesus/Downloads/bitmap/cmake-build-debug/CMakeFiles/bitmap.dir/BMInfoHeader.cpp.o"
+  "/Users/ragycostadejesus/Downloads/bitmap/BMReader.cpp" "/Users/ragycostadejesus/Downloads/bitmap/cmake-build-debug/CMakeFiles/bitmap.dir/BMReader.cpp.o"
+  "/Users/ragycostadejesus/Downloads/bitmap/BMWriter.cpp" "/Users/ragycostadejesus/Downloads/bitmap/cmake-build-debug/CMakeFiles/bitmap.dir/BMWriter.cpp.o"
+  "/Users/ragycostadejesus/Downloads/bitmap/Pixel.cpp" "/Users/ragycostadejesus/Downloads/bitmap/cmake-build-debug/CMakeFiles/bitmap.dir/Pixel.cpp.o"
+  "/Users/ragycostadejesus/Downloads/bitmap/PixelArray.cpp" "/Users/ragycostadejesus/Downloads/bitmap/cmake-build-debug/CMakeFiles/bitmap.dir/PixelArray.cpp.o"
+  "/Users/ragycostadejesus/Downloads/bitmap/bitmap.cpp" "/Users/ragycostadejesus/Downloads/bitmap/cmake-build-debug/CMakeFiles/bitmap.dir/bitmap.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
